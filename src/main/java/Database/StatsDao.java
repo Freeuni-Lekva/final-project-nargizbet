7bbdcd70c36c763d8cc89b6454952b02d990ce35
A-Kila <akila19@freeuni.edu.ge>
2021-08-06 16:45:24 +0400

    added template

--- src/main/java/Database/StatsDao.java
+++ src/main/java/Database/StatsDao.java
@@ -1,5 +1,30 @@
 package java.Database;
 
+import java.util.List;
+import javax.sql.DataSource;
+
 public class StatsDao {
 
+	private DataSource dataSource;
+	
+	public StatsDao(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
+	public synchronized int getWins() {
+		return 0;	
+	}
+	
+	public synchronized void addWins() {
+		
+	}
+	
+//	public synchronized List<Pair<User, Integer> getLeaderboard() {
+//		return null;
+//	}
+	
+	public synchronized int getUserPlace() {
+		return 0;
+	}
+	
 }
