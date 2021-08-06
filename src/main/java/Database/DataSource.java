3c8d177a274d4577049b4399c18534cc585cd466
wxmoiqwx <zchil19@freeuni.edu.ge>
2021-08-06 17:53:10 +0400

    Database Connection

---
+++ src/main/java/Database/DataSource.java
@@ -0,0 +1,18 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DataSource {
+    private static Connection con;
+
+    public static Connection getCon(){
+        try{
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306", "", "");
+            return con;
+        }catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+}
