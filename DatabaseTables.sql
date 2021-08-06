f9ffef4ce3108511d11b3e8d53338228c7b7e647
Guga Katamadze <71348577+GugaKatamadze@users.noreply.github.com>
2021-08-06 16:03:24 +0400

    Update DatabaseTables.sql

--- DatabaseTables.sql
+++ DatabaseTables.sql
@@ -1,6 +1,11 @@
 USE nargizbet;
-
+ 
+DROP TABLE IF EXISTS balance;
+DROP TABLE IF EXISTS friends;
+DROP TABLE IF EXISTS blackjack;
+DROP TABLE IF EXISTS slots;
 DROP TABLE IF EXISTS users;
+
 CREATE TABLE users (
 	username VARCHAR(64) PRIMARY KEY,
     first_name VARCHAR(64) NOT NULL,
@@ -9,15 +14,13 @@ CREATE TABLE users (
     member_since DATE NOT NULL,
     password VARCHAR(256) NOT NULL
 );
- 
-DROP TABLE IF EXISTS balance;
+
 CREATE TABLE balance (
 	username VARCHAR(64),
     balance DOUBLE,
     FOREIGN KEY(username) REFERENCES users(username)
 );
 
-DROP TABLE IF EXISTS friends;
 CREATE TABLE friends (
 	username1 VARCHAR(64),
     username2 VARCHAR(64),
@@ -25,14 +28,12 @@ CREATE TABLE friends (
     FOREIGN KEY(username1) REFERENCES users(username)
 );
 
-DROP TABLE IF EXISTS blackjack;
 CREATE TABLE blackjack (
 	username VARCHAR(64),
     wins INT,
     FOREIGN KEY(username) REFERENCES users(username)
 );
 
-DROP TABLE IF EXISTS slots;
 CREATE TABLE slots (
 	username VARCHAR(64),
     money_won DOUBLE,
