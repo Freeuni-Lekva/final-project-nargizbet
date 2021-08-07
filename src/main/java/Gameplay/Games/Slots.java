f8398e220df6255ad37d99adcc6c5314c543f731
wxmoiqwx <zchil19@freeuni.edu.ge>
2021-08-07 18:56:01 +0400

    Made ProfileServlet

---
+++ src/main/java/Gameplay/Games/Slots.java
@@ -0,0 +1,18 @@
+package Gameplay.Games;
+
+public class Slots implements Game{
+    @Override
+    public String getName() {
+        return "Slots";
+    }
+
+    @Override
+    public int getCapacity() {
+        return 1;
+    }
+
+    @Override
+    public String getDataBaseName() {
+        return "slots";
+    }
+}
