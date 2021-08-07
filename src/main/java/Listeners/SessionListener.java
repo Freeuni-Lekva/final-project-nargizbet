7d35cf7da2a5f5f3a5112e99c6e405c1d69a94fb
Gnems19 <71854341+Gnems19@users.noreply.github.com>
2021-08-07 18:14:35 +0400

    Update SessionListener.java

--- src/main/java/Listeners/SessionListener.java
+++ src/main/java/Listeners/SessionListener.java
@@ -8,23 +8,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 @WebListener()
 public class SessionListener implements HttpSessionListener {
 
-    private final AtomicInteger activeSessions;
-
-    public SessionListener() {
-        activeSessions = new AtomicInteger();
-    }
-
     @Override
     public void sessionCreated(HttpSessionEvent se){
-        activeSessions.incrementAndGet();
         se.getSession().setAttribute("User",null);
     }
-    @Override
-    public void sessionDestroyed(HttpSessionEvent se) {
-        activeSessions.decrementAndGet();
-    }
-    public int getActiveSessions(){
-        return activeSessions.get();
-    }
-
 }
