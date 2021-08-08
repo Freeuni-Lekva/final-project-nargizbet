d0a3676cda150b5a16a0446ecea974ce5d113001
wxmoiqwx <zchil19@freeuni.edu.ge>
2021-08-08 16:49:56 +0400

    savedpoint

--- src/main/java/Servlets/ProfileServlet.java
+++ src/main/java/Servlets/ProfileServlet.java
@@ -15,10 +15,6 @@ import java.io.IOException;
 import java.util.List;
 
 public class ProfileServlet extends HttpServlet {
-    public final static int MY_PROFILE = 0;
-    public final static int FRIEND_PROFILE = 1;
-    public final static int NOT_FRIEND_PROFILE = 2;
-    public final static int NOT_REGISTERED_PROFILE = 3;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,30 +25,30 @@ public class ProfileServlet extends HttpServlet {
         String username = currentUser.getUsername();
         String givenUsername = req.getParameter("Username");
         User givenUser = new User(givenUsername, "", "", "");
-        if(!UDAO.userRegistered(givenUser)){
-            req.setAttribute("username", givenUsername);
-            req.setAttribute("ProfileType", NOT_REGISTERED_PROFILE);
-        }else if(username.equals(givenUsername) || currentUser.isFriendsWith(givenUser)){
-                if(currentUser.isFriendsWith(givenUser)) currentUser = UDAO.getUser(givenUsername);
-                List<User> friendsList = friends.getFriends(currentUser);
-                req.setAttribute("FriendsList", friendsList);
-                req.setAttribute("ProfilePicture", currentUser.getProfilePicture());
-                req.setAttribute("first_name", currentUser.getFirstName());
-                req.setAttribute("last_name", currentUser.getLastName());
-                Game bj = new Blackjack();
-                Game slots = new Slots();
-                req.setAttribute("BJWins", stats.getWins(currentUser, bj));
-                req.setAttribute("SlotMoneyGambled", stats.getWins(currentUser, slots));
-                req.setAttribute("MemberSince", currentUser.getMemberSince());
-                if(username.equals(givenUsername)) req.setAttribute("ProfileType", MY_PROFILE);
-                else req.setAttribute("ProfileType", FRIEND_PROFILE);
+        if(username.equals(givenUsername) || currentUser.isFriendsWith(givenUser)){
+            boolean isMyProfile = true;
+            if(currentUser.isFriendsWith(givenUser)) {
+                currentUser = UDAO.getUser(givenUsername);
+                isMyProfile = false;
+            }
+            List<User> friendsList = friends.getFriends(currentUser);
+            req.setAttribute("FriendsList", friendsList);
+            req.setAttribute("ProfilePicture", currentUser.getProfilePicture());
+            req.setAttribute("first_name", currentUser.getFirstName());
+            req.setAttribute("last_name", currentUser.getLastName());
+            Game bj = new Blackjack();
+            Game slots = new Slots();
+            req.setAttribute("BJWins", stats.getWins(currentUser, bj));
+            req.setAttribute("SlotMoneyGambled", stats.getWins(currentUser, slots));
+            req.setAttribute("MemberSince", currentUser.getMemberSince());
+            req.getServletContext().getRequestDispatcher("/profile.jsp").forward(req, resp);
         }else {
             User usr = UDAO.getUser(givenUsername);
             req.setAttribute("first_name", usr.getFirstName());
             req.setAttribute("last_name", usr.getLastName());
-            req.setAttribute("ProfileType", NOT_FRIEND_PROFILE);
+            req.setAttribute("ProfilePicture", usr.getProfilePicture());
+            req.getServletContext().getRequestDispatcher("/NotFriendProfile.jsp").forward(req, resp);
         }
-        req.getServletContext().getRequestDispatcher("/profile.jsp").forward(req, resp);
     }
 
 
