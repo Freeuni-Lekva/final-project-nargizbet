78b6dbbb2be195182c4fde987d3b15552182241e
Guga Katamadze <gkata19@freeuni.edu.ge>
2021-08-09 16:49:29 +0400

    RegisterServlet update and bug fix

--- src/main/java/Servlets/RegisterServlet.java
+++ src/main/java/Servlets/RegisterServlet.java
@@ -14,30 +14,17 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        UserDAO UDAO = (UserDAO)request.getServletContext().getAttribute("UserDAO");
-        BalanceDAO BDAO = (BalanceDAO)request.getServletContext().getAttribute("BalanceDAO");
+        UserDAO UDAO = (UserDAO) request.getServletContext().getAttribute("UserDAO");
+        BalanceDAO BDAO = (BalanceDAO) request.getServletContext().getAttribute("BalanceDAO");
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
         String username = request.getParameter("username");
         String password = request.getParameter("psw");
         User user = new User(username, password, firstName, lastName);
-
-        if (UDAO.userRegistered(user)) {
-            request.setAttribute("ErrorMessage", "Provided username is already taken");
-            request.getRequestDispatcher("TryRegisterAgain.jsp").forward(request, response);
-        } else if (user.getPassword().length() < User.PASSWORD_MINIMUM_LENGTH) {
-            String errorMessage = "Password must contain at least " + User.PASSWORD_MINIMUM_LENGTH + " symbols";
-            request.setAttribute("ErrorMessage", errorMessage);
-            request.getRequestDispatcher("TryRegisterAgain.jsp").forward(request, response);
-        } else {
-            UDAO.addUser(user);
-            BDAO.addBalance(user);
-
-            request.getSession().setAttribute("User", user);
-            request.getRequestDispatcher("HomepageServlet").forward(request, response);
-        }
-
+        UDAO.addUser(user);
+        BDAO.addBalance(user);
+        request.getSession().setAttribute("User", user);
+        request.getRequestDispatcher("HomepageServlet").forward(request, response);
     }
 
 }
