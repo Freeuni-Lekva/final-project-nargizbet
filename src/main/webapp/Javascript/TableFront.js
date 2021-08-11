c8b0e6a2e012991a53f9e04d830f01b135ad2128
A-Kila <akila19@freeuni.edu.ge>
2021-08-11 19:15:14 +0400

    implement blackjack buttons

--- src/main/webapp/Javascript/TableFront.js
+++ src/main/webapp/Javascript/TableFront.js
@@ -1,6 +1,7 @@
+// for chat client
 const addMessage = (user, message) => {
     const maxMessages = 10;
-    
+
     const messageWindow = document.querySelector(".messageWindow");
     const messageCount = messageWindow.childElementCount;
 
@@ -10,4 +11,30 @@ const addMessage = (user, message) => {
     }
 
     messageWindow.innerHTML += `<p class="message">${user}: ${message}</p>`;
+}
+
+// for blackjack client
+const setAmount = (amount) => {
+    const amountWindow = document.querySelector(".amountLable");
+
+    amountWindow.innerHTML += amount;
+}
+
+const drawActionButtons = (onClickHit, onClickStand) => {
+    const hitButton = document.querySelector(".hitBtn");
+    const standButton = document.querySelector(".standBtn");
+
+    hitButton.onclick = onClickHit;
+    standButton.onclick = onClickStand;
+
+    hitButton.hidden = false;
+    standButton.hidden = false;
+}
+
+const removeActionButtons = () => {
+    const hitButton = document.querySelector(".hitBtn");
+    const standButton = document.querySelector(".standBtn");
+
+    hitButton.hidden = true;
+    standButton.hidden = true;
 }
